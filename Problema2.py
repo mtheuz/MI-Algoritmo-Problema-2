2912d2f7b2759dcfd5ac6c97a7d0ed17c825c28f
Matheus Mota <teus_030@hotmail.com>
2022-02-26 19:01:23 -0300

    Update Problema2.py

--- Problema2.py
+++ Problema2.py
@@ -1,11 +1,3 @@
-#Autor: Matheus Mota Santos
-#Componente Curricular: EXA854 MI-Algoritmo
-#Concluido em: 20/10/2021
-#Declaro que este código foi elaborado por mim de forma individual e não contém nenhum
-#trecho de código de outro colega ou de outro autor, tais como provindos de livros e
-#apostilas, e páginas ou documentos eletrônicos da Internet. Qualquer trecho de código
-#de outra autoria que não a minha está destacado com uma citação para o autor e a fonte
-#do código, e estou ciente que estes trechos não serão considerados para fins de avaliação.
 
 import string, random, copy
 from time import sleep
